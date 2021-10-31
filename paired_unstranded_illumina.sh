c95751d29ea7278f8741ac0057de37f6f5a61f29
Lina <57382343+herlianal12@users.noreply.github.com>
2021-10-31 17:18:14 +1030

    Delete paired_unstranded_illumina.sh

--- paired_unstranded_illumina.sh
+++
@@ -1,5 +0,0 @@
-wget "https://universityofadelaide.box.com/shared/static/obhco9tq57wxwht6oglktlvzz5ps5b9v.gz" \
--O paired_unstranded_illumina/raw_reads/SRR629688_R1_001.fastq.gz
-
-wget "https://universityofadelaide.box.com/shared/static/bsn5h8yr8p6k6d078ufn7txhwxp9g5gd.gz" \
--O paired_unstranded_illumina/raw_reads/SRR629688_R2_001.fastq.gz
