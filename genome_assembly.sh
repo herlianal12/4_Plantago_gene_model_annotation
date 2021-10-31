ce532187cba7f28427f97b6dd41dfdbc2a9a5cf5
Lina <57382343+herlianal12@users.noreply.github.com>
2021-10-31 17:08:28 +1030

    Delete genome_assembly.sh

--- genome_assembly.sh
+++
@@ -1,3 +0,0 @@
-wget "https://universityofadelaide.box.com/shared/static/vva52f3ysxqaxbcfowo9xjjy2wstosws.gz" \
--O references/Plantago.fasta.gz &&
-cp references/Plantago.fasta.gz assembly/index_HiC/Plantago.fasta.gz
